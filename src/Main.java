72e800e2cd0c0b8a90794264a9eba5be1f133491
gamiv <vrajgami100@gmail.com>
2023-02-16 18:06:21 -0800

    just checking conflict with main

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello humans on this world!");
+        System.out.println("Hello humans on this planet!");
         //Does it work?
         // helloo this is a stupid comment
 
